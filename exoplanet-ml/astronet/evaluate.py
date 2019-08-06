71590e59e9c38357385ff3528a91a189a9cf4ed9
Chris Shallue <shallue@google.com>
2019-08-06 16:09:25 -0700

    Update astronet/evaluate.py to use the new eval_args format.
    PiperOrigin-RevId: 262017013

--- exoplanet-ml/astronet/evaluate.py
+++ exoplanet-ml/astronet/evaluate.py
@@ -87,9 +87,8 @@ def main(_):
 
   # Run evaluation. This will log the result to stderr and also write a summary
   # file in the model_dir.
-  eval_steps = None  # Evaluate over all examples in the file.
-  eval_args = {FLAGS.eval_name: (input_fn, eval_steps)}
-  estimator_runner.evaluate(estimator, eval_args)
+  eval_args = {"name": FLAGS.eval_name, "input_fn": input_fn}
+  estimator_runner.evaluate(estimator, [eval_args])
 
 
 if __name__ == "__main__":
